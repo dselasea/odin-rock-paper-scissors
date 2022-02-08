29bf7b916a30ea38680b2ab67b5ffb566a8907e0
Daniel Selase Anyidoho <dselasea@gmail.com>
2022-02-08 12:33:47 +0000

    Added game function and variables to keep the scores

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-function computerPlay(){
+let player = 0;
+let comp = 0;
+const computerSelection = function computerPlay(){
     let compChoice = ["rock", "paper", "scissors"];
     let num = Math.floor(Math.random() * 3);
-    // console.log(compChoice[num]);
+    console.log(compChoice[num]);
     return compChoice[num];
 }
 
-function playerSelection(){
+const playerSelection = function playerSelection(){
     let playerChoice = prompt("Rock, Paper, or Scissors?");
     let choice = playerChoice.trim().toLowerCase();
+    console.log(choice);
     return choice;
 }
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection === "rock" && computerSelection === "scissors"){
         console.log("Rock wins!");
+        player++;
     }else if(playerSelection === "rock" && computerSelection === "paper"){
         console.log("Computer wins!");
+        comp++;
     }else if(playerSelection === "paper" && computerSelection === "scissors"){
         console.log("Computer wins!");
+        comp++;
     }else if(playerSelection === "paper" && computerSelection === "rock"){
         console.log("Paper wins!");
+        player++;
     }else if(playerSelection === "scissors" && computerSelection === "rock"){
         console.log("Computer wins!");
+        comp++;
     }else if(playerSelection === "scissors" && computerSelection === "paper"){
-        console.log("Scissors wins!")
+        console.log("Scissors wins!");
+        player++;
     }else {
         console.log("It's a tie!");
     }
 }
 
-playerSelection();
-computerPlay();
+function game(){
+    let count = 0;
+    while(count <= 5){
+        playRound(playerSelection(), computerSelection());
+        count++;
+    }
+    if(player > comp){
+        console.log("Player wins!")
+    }else if(player < comp){
+        console.log("Computer wins!");
+    }else{
+        console.log("Its a tie!")
+    }
+    console.log(player, comp);
+}
+// console.log(playerSelection());
+// console.log(computerSelection());
+game();
