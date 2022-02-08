9530593f56d82d6bc21a3d92091075a14413ce36
Daniel Selase Anyidoho <dselasea@gmail.com>
2022-02-08 11:41:12 +0000

    added player and computer functions

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ function computerPlay(){
     return compChoice[num];
 }
 
-function playRound(playerSelection, computerSelection){
-
-}
-
 function playerSelection(){
     let playerChoice = prompt("Rock, Paper, or Scissors?");
     let choice = playerChoice.trim().toLowerCase();
-    console.log(choice);
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("Rock wins!");
+    }else if(playerSelection === "rock" && computerSelection === "paper"){
+        console.log("Computer wins!");
+    }else if(playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("Computer wins!");
+    }else if(playerSelection === "paper" && computerSelection === "rock"){
+        console.log("Paper wins!");
+    }else if(playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("Computer wins!");
+    }else if(playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("Scissors wins!")
+    }else {
+        console.log("It's a tie!");
+    }
 }
 
 playerSelection();
